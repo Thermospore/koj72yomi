0b64a090adda4ad1a69265b362fba125b62fc4c0
Thermospore <35350819+Thermospore@users.noreply.github.com>
2021-07-04 07:10:37 -0800

    termbank writing test

--- koj72yomi.cpp
+++ koj72yomi.cpp
@@ -1,1 +1,16 @@
-yeet
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+  // Create and open term bank file
+  ofstream termBank("term_bank_1.json");
+
+  // Test write
+  termBank << "here is where the yomi dict is gonna go (Wow!!)";
+
+  // Close the file
+  termBank.close();
+
+	return 0;
+}
